2f37269502fb5a616b1ab34da4c6f7322aa6ca3d
YurasovAleksey <alex286ur@rambler.ru>
2025-07-18 02:13:13 +0300

    fixed app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -19,62 +19,60 @@ from .url_repository import UrlRepository
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv("SECRET_KEY")
 
 
 def get_db_connection():
     return psycopg2.connect(
-        os.getenv('DATABASE_URL'),
-        cursor_factory=NamedTupleCursor
+        os.getenv("DATABASE_URL"), cursor_factory=NamedTupleCursor
     )
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/urls', methods=['GET'])
+@app.route("/urls", methods=["GET"])
 def urls():
     conn = get_db_connection()
     repo = UrlRepository(conn)
     urls_list = repo.get_all_urls()
     conn.close()
-    return render_template('urls.html', urls=urls_list)
+    return render_template("urls.html", urls=urls_list)
 
 
-@app.route('/urls', methods=['POST'])
+@app.route("/urls", methods=["POST"])
 def add_url():
-    raw_url = request.form.get('url')
-    if not raw_url:
-        flash('URL обязателен', 'danger')
-        return render_template('index.html'), 422
-
+    raw_url = request.form.get("url")
     conn = get_db_connection()
     try:
         repo = UrlRepository(conn)
         is_added, url_id, message = repo.add_url(raw_url)
-        
+
         if not url_id:
-            flash(message, 'danger')
-            return render_template('index.html'), 422
-            
+            flash(message, "danger")
+            return render_template(
+                "index.html",
+                messages=get_flashed_messages(with_categories=True),
+            ), 422
+
         if is_added:
-            flash(message, 'success')
+            flash(message, "success")
         else:
-            flash(message, 'info')
-            
-        return redirect(url_for('show_url', id=url_id))
-        
+            flash(message, "info")
+
+        return redirect(url_for("show_url", id=url_id))
+
     except Exception as e:
-        flash(f"Произошла ошибка: {str(e)}", 'danger')
-        return render_template('index.html'), 500
-        
+        flash(f"Произошла ошибка: {str(e)}", "danger")
+        return render_template("index.html"), 500
+
     finally:
         conn.close()
 
 
-@app.route('/urls/<int:id>')
+@app.route("/urls/<int:id>")
 def show_url(id):
     conn = get_db_connection()
     repo = UrlRepository(conn)
@@ -85,13 +83,11 @@ def show_url(id):
     checks = repo.get_checks_url(id)
     messages = get_flashed_messages(with_categories=True)
     return render_template(
-        'url.html',
-        url=url,
-        checks=checks,
-        messages=messages)
+        "url.html", url=url, checks=checks, messages=messages
+    )
 
 
-@app.route('/urls/<int:id>/checks', methods=['POST'])
+@app.route("/urls/<int:id>/checks", methods=["POST"])
 def check_url(id):
     conn = get_db_connection()
     try:
@@ -99,20 +95,18 @@ def check_url(id):
         success, check_id, message = repo.add_check(id)
 
         if not success:
-            flash(message, 'danger')
-            return redirect(url_for('show_url', id=id))
+            flash(message, "danger")
+            return redirect(url_for("show_url", id=id))
 
         flash("Страница успешно проверена", "success")
-        return redirect(url_for('show_url', id=id))
+        return redirect(url_for("show_url", id=id))
     except Exception as e:
         flash(f"Произошла ошибка при проверке: {str(e)}", "danger")
-        return redirect(url_for('show_url', id=id))
+        return redirect(url_for("show_url", id=id))
     finally:
         conn.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template(
-        'page_not_found.html'
-    ), 404
+    return render_template("page_not_found.html"), 404
